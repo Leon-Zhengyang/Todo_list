c41709a3d5eb232e1504ecd0ec50be2cf2e3b421
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2023-01-26 22:26:56 +0900

    test

--- Todolist/tests/test_ex1.py
+++ Todolist/tests/test_ex1.py
@@ -6,30 +6,23 @@ from Todolist.models import Todo, Priority
 def create_todo():
       Priority.objects.create(label="高")
       priority_id = Priority.objects.get(label="高")
-      Todo.objects.create(
+      todo = Todo.objects.create(
             task="test",
             date_start="2022-10-11",
             date_limit="2022-10-12",
             priority=priority_id,
             comment="just a test"     
       )
+      return todo
 
 @pytest.mark.django_db
 def test_1(create_todo):
-      create_todo()
-      create_todo()
-      assert Todo.objects.count() == 2
+      create_todo
+      assert Todo.objects.count() == 1
 
 @pytest.mark.django_db
-def test_2():
-      Priority.objects.create(label="高")
-      priority_id = Priority.objects.get(label="高")
-      Todo.objects.create(
-            task="test",
-            date_start="2022-10-11",
-            date_limit="2022-10-12",
-            priority=priority_id,
-            comment="just a test"
-      )
+def test_2(create_todo):
+      todo = create_todo
+      assert todo.task == 'test'
       assert Todo.objects.count() == 1
 

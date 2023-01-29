c944cef5375ed914c6b9855a1c6def288c040b49
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2023-01-29 17:26:18 +0900

    factory introduction

--- Todolist/tests/test_ex1.py
+++ Todolist/tests/test_ex1.py
@@ -8,9 +8,11 @@ def test_todo_factory(db, todo_factory, priority_factory):
       high = priority_factory.create(label='高')
       priority_factory.create(label='中')
       low = priority_factory.create(label='低')
-      a = todo_factory.create(task="ppap", priority=low)
+      a = todo_factory
+      print(a.priority)
       b = todo_factory
       a.priority = high
+      print(a.priority.label)
       b.priority = low
       count = Priority.objects.all().count()
       assert count == 3

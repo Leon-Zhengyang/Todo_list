80c1711bde4dd53cba5c9d516759d7453f4fbba6
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2023-02-24 22:23:03 +0900

    add pytest

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Todolist.apps.TodolistConfig',
-    # "sslserver",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

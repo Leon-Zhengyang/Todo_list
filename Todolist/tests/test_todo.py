24934db8de4fa59e0675912a54c562e26c257d99
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2023-02-24 22:20:24 +0900

    add pytest

--- Todolist/tests/test_todo.py
+++ Todolist/tests/test_todo.py
@@ -1,6 +1,14 @@
 import pytest
 
 from Todolist.models import Todo, Priority
+from rest_framework.test import APIClient
+import pytest
+
+client = APIClient()
+@pytest.mark.django_db
+def test_get_song():
+    response = client.post('init_list/')
+    assert response.status_code == 200
 
 @pytest.fixture
 def priority_todo_factory(db, todo_factory, priority_factory):

c69cdb0083dad6c63dd6882cc65d3121aadde505
LAPTOP-74FCSJHK\liuzh <liuzhengyang689@gmail.com>
2023-02-25 21:09:25 +0900

    test-0225

--- Todolist/tests/test_todo.py
+++ Todolist/tests/test_todo.py
@@ -8,7 +8,7 @@ client = APIClient()
 @pytest.mark.django_db
 def test_url():
     response = client.post('init_list/', {'title': 'new idea'}, format='json')
-    assert response.status == 200
+    assert response.status_code == 200
 
 @pytest.fixture
 def priority_todo_factory(db, todo_factory, priority_factory):
